70e332027551787dd30ac9854b05a59b0affd99c
lucasaobarbosa <lucasaobarbosa@gmail.com>
2021-01-13 18:40:23 -0300

    Criando balões for

--- jogo.js
+++ jogo.js
@@ -1,25 +1,39 @@
-function iniciaJogo(){
-var url = window.location.search; /* Recupera o que está a partir do "?" Ex:?3 */
-var nivel_jogo = url.replace("?", ""); /* Substituindo o ? por nada */
+function iniciaJogo() {
+    var url = window.location.search; /* Recupera o que está a partir do "?" Ex:?3 */
+    var nivel_jogo = url.replace("?", ""); /* Substituindo o ? por nada */
 
-/* 1 Fácil = 120 segundos
-   2 Normal = 60 segundos
-   3 Difícil = 30 segundos */
+    /* 1 Fácil = 120 segundos
+       2 Normal = 60 segundos
+       3 Difícil = 30 segundos */
 
-var tempo_segundos = 0;
+    var tempo_segundos = 0;
 
-if(nivel_jogo ==1){
-    tempo_segundos = 120;
+    if (nivel_jogo == 1) {
+        tempo_segundos = 120;
+    }
+    if (nivel_jogo == 2) {
+        tempo_segundos = 60;
+    }
+
+    if (nivel_jogo == 3) {
+        tempo_segundos = 30;
+    }
+
+    /* Inserindo os segundos no span */
+    document.getElementById('cronometro').innerHTML = tempo_segundos; /*  <!-- Inserindo o cronometro dentro da imagem --> */
+
+    /* Quantidade de balões */
+    var qtde_baloes = 10;
+    cria_baloes(qtde_baloes);
 }
-if(nivel_jogo ==2){
-    tempo_segundos = 60;
-}
 
-if(nivel_jogo ==3){
-    tempo_segundos = 30;
-}
+/* Criando balões */
+function cria_baloes(qtde_baloes) {
 
-/* Inserindo os segundos no span */
-document.getElementById('cronometro').innerHTML = tempo_segundos; /*  <!-- Inserindo o cronometro dentro da imagem --> */
+    for (var i = 1; i <= qtde_baloes; i++) {
 
+        var balao = document.createElement("img"); /* Criando elementos 'igm' */
+        balao.src = 'imagens/balao_azul_pequeno.png';
+
+    }
 }
