72a0019bd45573e102ec4440061283765aae33a2
lucasaobarbosa <lucasaobarbosa@gmail.com>
2021-01-13 18:01:49 -0300

    Condição if

--- jogo.js
+++ jogo.js
@@ -1,6 +1,19 @@
 function iniciaJogo(){
 var url = window.location.search; /* Recupera o que está a partir do "?" Ex:?3 */
 var nivel_jogo = url.replace("?", ""); /* Substituindo o ? por nada */
-alert(nivel_jogo);
+
+/* 1 Fácil = 120 segundos
+   2 Normal = 60 segundos
+   3 Difícil = 30 segundos */
+var tempo_segundos = 0;
+if(nivel_jogo ==1){
+    tempo_segundos == 120;
+}
+if(nivel_jogo ==2){
+    tempo_segundos == 60;
+}
+if(nivel_jogo ==3){
+    nivel_jogo == 30;
+}
 
 }
