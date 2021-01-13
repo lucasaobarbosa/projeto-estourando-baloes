8528cbfbd47ddb861cbcda9a1ad38e14814f235e
lucasaobarbosa <lucasaobarbosa@gmail.com>
2021-01-13 18:11:48 -0300

    Segundos

--- jogo.js
+++ jogo.js
@@ -5,18 +5,20 @@ var nivel_jogo = url.replace("?", ""); /* Substituindo o ? por nada */
 /* 1 Fácil = 120 segundos
    2 Normal = 60 segundos
    3 Difícil = 30 segundos */
+
 var tempo_segundos = 0;
+
 if(nivel_jogo ==1){
-    tempo_segundos == 120;
+    tempo_segundos = 120;
 }
 if(nivel_jogo ==2){
-    tempo_segundos == 60;
+    tempo_segundos = 60;
 }
 if(nivel_jogo ==3){
-    nivel_jogo == 30;
+    nivel_jogo = 30;
 }
 
 /* Inserindo os segundos no span */
-document.getElementById('cronometro').innerHTML = tempo_segundos;
+document.getElementById('cronometro').innerHTML = tempo_segundos; /*  <!-- Inserindo o cronometro dentro da imagem --> */
 
 }
